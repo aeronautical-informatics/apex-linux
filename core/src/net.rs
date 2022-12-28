5e1400cc7dc3439b1c9fdd0abc504f69d5b05e59
Emil Engler <me@emilengler.com>
2022-12-28 10:52:25 +0100

    core: implement net::get_interfaces()
    This commit implements the get_interfaces() function, a function that
    returns all currently available interfaces in a Vec<String>.

--- core/src/net.rs
+++ core/src/net.rs
@@ -4,6 +4,7 @@ use std::process::Command;
 
 use anyhow::bail;
 use nix::unistd::Pid;
+use regex::Regex;
 
 /// Moves interface iname to the network namespace of PID
 pub fn move_to_ns(iname: &str, pid: Pid) -> anyhow::Result<()> {
@@ -17,4 +18,24 @@ pub fn move_to_ns(iname: &str, pid: Pid) -> anyhow::Result<()> {
     } else {
         bail!("{}", String::from_utf8(cmd.stderr)?)
     }
+}
+
+/// Returns all interfaces available in the current namespace
+pub fn get_interfaces() -> anyhow::Result<Vec<String>> {
+    let cmd = Command::new("sh").arg("-c").arg("ip address").output()?;
+    if !cmd.status.success() {
+        bail!("ip-address(8) failed")
+    }
+
+    let str = String::from_utf8(cmd.stdout)?;
+    let re = Regex::new(r"^\d+: (.+):")?;
+    let mut result: Vec<String> = Vec::new();
+
+    for line in str.lines() {
+        for cap in re.captures_iter(line) {
+            result.push(cap[1].to_string());
+        }
+    }
+
+    anyhow::Ok(result)
 }
